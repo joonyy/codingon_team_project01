ac29534d475fe7bd1ff9fc21ef955dc6c01e0367
Joonyoung <blackhound97@gmail.com>
2024-03-15 13:34:31 +0900

    category.js 정리

--- src/category.js
+++
@@ -1,5 +0,0 @@
-let text_height = document.getElementsByClassName('category-card-text')[0];
-
-text_height.style.height = () =>{
-  return $(".category-box-item").height() - $(".category-box-item > img").height();
-}

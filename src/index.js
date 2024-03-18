c07eaf44ba05f5ddf856b388f1e35d9cd32164b3
dongmyeong930213 <dongmyeong930213@gmail.com>
2024-03-18 13:35:37 +0900

    mypage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,63 @@
 console.log("working");
 
-const container = document.querySelector(".card-container");
-container.addEventListener("wheel", (event) => {
-  if(event.deltaY != 0) {
-    event.preventDefault();
-    container.scrollLeft += event.deltaY;
-  }
-});
+// Signin-popup, Login-popup 페이지 보이기, 숨기기 시작
+function LoginPopupOpen() {
+  var LoginPopup = document.querySelector('.login-popup-background');
+  LoginPopup.style.visibility = "visible";
+  LoginPopup.style.opacity = '1';
+}
 
-console.log("working");
+function LoginPopupClose() {
+  var LoginPopup = document.querySelector('.login-popup-background');
+  LoginPopup.style.visibility = "hidden";
+  LoginPopup.style.opacity = '0';
+}
+
+function SigninPopupOpen() {
+  var SigninPopup = document.querySelector('.signin-popup-background');
+  SigninPopup.style.visibility = "visible";
+  SigninPopup.style.opacity = '1';
+}
+
+function SigninPopupClose() {
+  var SigninPopup = document.querySelector('.signin-popup-background');
+  SigninPopup.style.visibility = "hidden";
+  SigninPopup.style.opacity = '0';
+}
+
+function SearchPopupOpen() {
+  var SigninPopup = document.querySelector('.search-popup-background');
+  SigninPopup.style.visibility = "visible";
+  SigninPopup.style.opacity = '1';
+}
+
+function SearchPopupClose() {
+  var LoginPopup = document.querySelector('.search-popup-background');
+  LoginPopup.style.visibility = "hidden";
+  LoginPopup.style.opacity = '0';
+}
+// Signin-popup, Login-popup 페이지 보이기, 숨기기 끝
+
+// 메인페이지 카드 가로 스크롤로 세로스크롤 이동하기 시작
+document.addEventListener("DOMContentLoaded", function() {
+  var containers = document.querySelectorAll('.body-main-page-card');
+
+  containers.forEach(function(container) {
+    container.addEventListener('wheel', function(event) {
+      event.preventDefault(); // 기본 스크롤 동작 방지
+
+      var delta = event.deltaY || event.detail || event.wheelDelta;
+
+      if (container.scrollLeft === 0 && delta < 0) {
+        // 가로 스크롤이 가장 왼쪽에 있고, 스크롤 방향이 위쪽으로 이동할 때
+        window.scrollBy({ top: delta, left: 0, behavior: 'smooth' });
+      } else if (container.scrollLeft >= (container.scrollWidth - container.clientWidth) && delta > 0) {
+        // 가로 스크롤이 가장 오른쪽에 있고, 스크롤 방향이 아래쪽으로 이동할 때
+        window.scrollBy({ top: delta, left: 0, behavior: 'smooth' });
+      } else {
+        container.scrollLeft += (delta > 0 ? 1 : -1) * 40; // 가로 스크롤 이동
+      }
+    });
+  });
+});
+// 메인페이지 카드 가로 스크롤로 세로스크롤 이동하기 끝
---
+++ src/mypage.js
@@ -0,0 +1,39 @@
+console.log("working");
+
+// Signin-popup, Login-popup 페이지 보이기, 숨기기 시작
+function LoginPopupOpen() {
+  var LoginPopup = document.querySelector('.login-popup-background');
+  LoginPopup.style.visibility = "visible";
+  LoginPopup.style.opacity = '1';
+}
+
+function LoginPopupClose() {
+  var LoginPopup = document.querySelector('.login-popup-background');
+  LoginPopup.style.visibility = "hidden";
+  LoginPopup.style.opacity = '0';
+}
+
+function SigninPopupOpen() {
+  var SigninPopup = document.querySelector('.signin-popup-background');
+  SigninPopup.style.visibility = "visible";
+  SigninPopup.style.opacity = '1';
+}
+
+function SigninPopupClose() {
+  var SigninPopup = document.querySelector('.signin-popup-background');
+  SigninPopup.style.visibility = "hidden";
+  SigninPopup.style.opacity = '0';
+}
+
+function SearchPopupOpen() {
+  var SigninPopup = document.querySelector('.search-popup-background');
+  SigninPopup.style.visibility = "visible";
+  SigninPopup.style.opacity = '1';
+}
+
+function SearchPopupClose() {
+  var LoginPopup = document.querySelector('.search-popup-background');
+  LoginPopup.style.visibility = "hidden";
+  LoginPopup.style.opacity = '0';
+}
+// Signin-popup, Login-popup 페이지 보이기, 숨기기 끝

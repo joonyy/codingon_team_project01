bf6b3e73e281749c6cc766e984a7f56569f93fdd
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-03-18 16:56:00 +0900

    item page css

--- src/index.js
+++ src/index.js
@@ -63,11 +63,10 @@ document.addEventListener("DOMContentLoaded", function() {
 // 메인페이지 카드 가로 스크롤로 세로스크롤 이동하기 끝
 
 // 찜하기 버튼 시작
-// const heart = document.querySelector(".bi-heart");
-// heart.addEventListener('click', function() {
-//   heart.classList.toggle("bi-heart-fill");
-//   heart.classList.toggle("bi-heart-charge");
-// });
+const heart = document.querySelector("#heart");
+heart.addEventListener('click', function() {
+  heart.classList.toggle("bi-heart-fill");
+});
 // 찜하기 버튼 끝
 
 // 사이즈 선택 시작
@@ -86,3 +85,33 @@ selectedcolor.addEventListener('input', () => {
   changedcolor.style.backgroundColor = selectColor;
 });
 // 색상 선택 끝
+
+// 상세정보 리뷰 선택 시작
+function select() {
+  const moreinfo = document.querySelector(".more-info-select");
+  const review = document.querySelector(".review-select");
+
+  if(moreinfo.classList.contains("selected") === true) {
+    moreinfo.classList.remove("selected");
+    review.classList.add("selected");
+  }
+  else {
+    moreinfo.classList.add("selected");
+    review.classList.remove("selected");
+  }
+}
+
+const button1 = document.getElementsByClassName("more-info-select")[0];
+const button2 = document.getElementsByClassName("review-select")[0];
+
+const section1 = document.querySelector(".item-details");
+const section2 = document.querySelector(".review");
+
+button1.addEventListener('click', () => {
+  window.scrollBy({top: section1.getBoundingClientRect().top, behavior: 'smooth'});
+});
+
+button2.addEventListener('click', () => {
+  window.scrollBy({top: section2.getBoundingClientRect().top, behavior: 'smooth'});
+});
+// 상세정보 리뷰 선택 끝

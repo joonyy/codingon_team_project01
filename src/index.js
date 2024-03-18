baba7b099b19e8ea814c5b30190a9e881b4e0100
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-03-18 19:13:06 +0900

    item page wansung

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ heart.addEventListener('click', function() {
 // 사이즈 선택 시작
 function selectSize(size) {
   let selectedSize = document.getElementById("selectedSize");
-  selectedSize.innerHTML = "size : " + size + "mm";
+  selectedSize.innerHTML = "size : " + size;
 }
 // 사이즈 선택 끝
 
@@ -87,20 +87,6 @@ selectedcolor.addEventListener('input', () => {
 // 색상 선택 끝
 
 // 상세정보 리뷰 선택 시작
-function select() {
-  const moreinfo = document.querySelector(".more-info-select");
-  const review = document.querySelector(".review-select");
-
-  if(moreinfo.classList.contains("selected") === true) {
-    moreinfo.classList.remove("selected");
-    review.classList.add("selected");
-  }
-  else {
-    moreinfo.classList.add("selected");
-    review.classList.remove("selected");
-  }
-}
-
 const button1 = document.getElementsByClassName("more-info-select")[0];
 const button2 = document.getElementsByClassName("review-select")[0];
 
@@ -109,9 +95,13 @@ const section2 = document.querySelector(".review");
 
 button1.addEventListener('click', () => {
   window.scrollBy({top: section1.getBoundingClientRect().top, behavior: 'smooth'});
+  button1.classList.add("selected");
+  button2.classList.remove("selected");
 });
 
 button2.addEventListener('click', () => {
   window.scrollBy({top: section2.getBoundingClientRect().top, behavior: 'smooth'});
+  button1.classList.remove("selected");
+  button2.classList.add("selected");
 });
 // 상세정보 리뷰 선택 끝

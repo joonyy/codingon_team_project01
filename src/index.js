f73b715a4380798b011a15846e00c54d67b267c1
Joonyoung <blackhound97@gmail.com>
2024-03-19 13:52:39 +0900

    index.css, index.js에 병합

--- src/index.js
+++ src/index.js
@@ -132,3 +132,81 @@ if (checklogin(userId, userPw)) {
 else {
   alert("아이디 혹은 비밀번호가 잘못된거 같아요!");
 }
+
+function confirmed(){
+  if(document.getElementById('where').value === "") {
+    alert('주소를 입력하세요!')
+  };
+  if(document.getElementById('who').value===""){
+    alert("이름을 입력하세요!")
+  };
+
+  if(document.getElementById('where').value != "" && document.getElementById('who').value!==""){
+    var ship_info = document.querySelector('.ship-info');
+    var ship_confirmed_info = document.querySelector('.ship-confirmed-info');
+    ship_info.style.visibility = "hidden";
+    ship_info.style.display="none";
+    ship_confirmed_info.style.visibility = "visible";
+    ship_confirmed_info.style.display = "flex";
+    const text = document.getElementById('where').value +" (으)로 선물해드릴게요 " + document.getElementById('who').value +"님!";
+    const newSpan = document.createElement('span');
+    newSpan.textContent=text;
+    var ship_confirmed_box = document.querySelector(".ship-confirmed-box");
+    ship_confirmed_box.innerHTML = ''; // 기존 내용 지우기
+    ship_confirmed_box.appendChild(newSpan);
+  }
+}
+// cart-page.js
+document.querySelector('#checkAll');
+
+checkAll.addEventListener('click', function(){
+  const isChecked = checkAll.checked;
+
+  if(isChecked){
+    const checkboxes = document.querySelectorAll('.check');
+
+    for(const checkbox of checkboxes){
+      checkbox.checked=true;
+    }
+  }
+  else{
+    const checkboxes = document.querySelectorAll('.check');
+    for(checkbox of checkboxes){
+      checkbox.checked =false;
+    }
+  }
+})
+
+function count1(type) {
+  const resultElement = document.getElementsByClassName('quantity-result')[0];
+
+  let number = resultElement.innerText;
+  if(type==='plus') number = parseInt(number) + 1;
+  if(type==='minus') {
+    if(number>0)number = parseInt(number) -1;
+  }
+
+  resultElement.innerText = number;
+}
+function count2(type) {
+  const resultElement = document.getElementsByClassName('quantity-result')[1];
+
+  let number = resultElement.innerText;
+  if(type==='plus') number = parseInt(number) + 1;
+  if(type==='minus') {
+    if(number>0)number = parseInt(number) -1;
+  }
+
+  resultElement.innerText = number;
+}
+function count3(type) {
+  const resultElement = document.getElementsByClassName('quantity-result')[2];
+
+  let number = resultElement.innerText;
+  if(type==='plus') number = parseInt(number) + 1;
+  if(type==='minus') {
+    if(number>0)number = parseInt(number) -1;
+  }
+
+  resultElement.innerText = number;
+}

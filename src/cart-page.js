a5f96ed34a78c3440436b4cf3876caad33e02b56
Joonyoung <blackhound97@gmail.com>
2024-03-19 11:59:13 +0900

    장바구니 페이지 완성

--- src/cart-page.js
+++ src/cart-page.js
@@ -17,3 +17,37 @@ checkAll.addEventListener('click', function(){
     }
   }
 })
+
+function count1(type) {
+  const resultElement = document.getElementsByClassName('quantity-result')[0];
+
+  let number = resultElement.innerText;
+  if(type==='plus') number = parseInt(number) + 1;
+  if(type==='minus') {
+    if(number>0)number = parseInt(number) -1;
+  }
+
+  resultElement.innerText = number;
+}
+function count2(type) {
+  const resultElement = document.getElementsByClassName('quantity-result')[1];
+
+  let number = resultElement.innerText;
+  if(type==='plus') number = parseInt(number) + 1;
+  if(type==='minus') {
+    if(number>0)number = parseInt(number) -1;
+  }
+
+  resultElement.innerText = number;
+}
+function count3(type) {
+  const resultElement = document.getElementsByClassName('quantity-result')[2];
+
+  let number = resultElement.innerText;
+  if(type==='plus') number = parseInt(number) + 1;
+  if(type==='minus') {
+    if(number>0)number = parseInt(number) -1;
+  }
+
+  resultElement.innerText = number;
+}

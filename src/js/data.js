d1f0d875208be633f7c6539b59e55be5b4c03174
Joonyoung <blackhound97@gmail.com>
2024-03-21 09:16:55 +0900

    data 틀 생성

--- src/js/data.js
+++ src/js/data.js
@@ -1,5 +1,4 @@
 let Data = [];
-
 //신발 객체 틀을 생성하는 함수
 const shoes = function(imgNum, name, price, contents, reviews){
   this.imgUrl = (`../img/shoes${imgNum}.png`);
@@ -9,5 +8,6 @@ const shoes = function(imgNum, name, price, contents, reviews){
   this.reviews = reviews;
 };
 for(let i=1;i<=20;i++){
-Data[i-1] = new shoes(i);
+  Data[i-1] = new shoes(i);
 }
+console.log(Data[1])

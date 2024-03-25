c61d0565a928098b88d300f8310c81fcd452fc23
ningdol0711 <160688506+ningdol0711@users.noreply.github.com>
2024-03-25 09:18:55 +0900

    project end

---
+++ src/main-page.js
@@ -0,0 +1,96 @@
+// 메인페이지 카드 가로 스크롤로 세로스크롤 이동하기 시작
+document.addEventListener("DOMContentLoaded", function() {
+  var containers = document.querySelectorAll('.body-main-page-card');
+
+  containers.forEach(function(container) {
+    container.addEventListener('wheel', function(event) {
+      event.preventDefault();
+
+      var delta = event.deltaY || event.detail || event.wheelDelta;
+
+      if (container.scrollLeft === 0 && delta < 0) {
+        window.scrollBy({ top: delta, left: 0});
+      } 
+      else if (container.scrollLeft >= (container.scrollWidth-container.clientWidth-1) && delta > 0) {
+        window.scrollBy({ top: delta, left: 0});
+      } 
+      else {
+        container.scrollLeft += (delta > 0 ? 1 : -1) * 40;
+      }
+    });
+  });
+});
+// 가로 스크롤 끝
+
+// main-page-card 다시 만들기 시작
+let catnum = 0;
+let countedcat = [];
+function shoecat() {
+  for(let i=0;i<Data.length;i++) {
+    if(Data[i].cat && !countedcat.includes(Data[i].cat)) {
+      catnum ++;
+      countedcat.push(Data[i].cat);
+    }
+  }
+}
+
+shoecat();
+
+let numberOfCards = {};
+for(let i=0;i<Data.length;i++) {
+  let item = Data[i];
+  let category = item.cat;
+
+  if(!numberOfCards[category]) {
+    numberOfCards[category] = [];
+  }
+  
+  numberOfCards[category].push(i);
+}
+
+const main = document.querySelector("main");
+
+for (let i = 0; i < catnum; i++) {
+  const mainCardContainer = document.createElement('section');
+  mainCardContainer.classList.add('body-main-page-card');
+  mainCardContainer.classList.add('main-card-container');
+  
+  const cardContainer = document.createElement('div');
+  cardContainer.classList.add('card-container');
+
+  const nowlocation = window.location.pathname;
+  const nowpage = nowlocation.split("/");
+
+  for (let j = 0; j < numberOfCards[countedcat[i]].length; j++) {
+    let num = numberOfCards[countedcat[i]][j] + 1;
+   
+    let cardHtml = `
+      <div class="main-card">
+        <a href="${nowpage[nowpage.length - 1].includes('login') ? 'item-page-login.html?data=' + encodeURIComponent(JSON.stringify(Data[num-1])) : 'item-page-logout.html?data=' + encodeURIComponent(JSON.stringify(Data[num-1]))}">
+          <img src="./shoes${num}.png" alt="shoes${num}">
+        </a>
+        <div class="main-card-info-box">
+          <a href="${nowpage[nowpage.length - 1].includes('login') ? 'item-page-login.html?data=' + encodeURIComponent(JSON.stringify(Data[num-1])) : 'item-page-logout.html?data=' + encodeURIComponent(JSON.stringify(Data[num-1]))}" class="text-btn card-text-btn">
+            <div class="main-card-info main-card-info-head">
+              <span>${Data[num-1].name}</span>
+            </div>
+            <div class="main-card-info main-card-info-body">
+              <div class="sizes">240 245 250 255 260 265 270 275 280 285</div>
+              <div class="colors">
+                RED BLACK WHITE IVORY
+              </div>
+            </div>
+            <div class="main-card-info main-card-info-footer">
+            <p>${Data[num-1].price}깃털</p>
+            </div>
+          </a>
+        </div>
+      </div>`;
+
+    cardContainer.innerHTML += cardHtml;
+  }
+
+  mainCardContainer.appendChild(cardContainer);
+  main.appendChild(mainCardContainer);
+}
+// main-page-card 다시 만들기 끝
---
+++ src/mypage.js
@@ -0,0 +1,125 @@
+console.log("working");
+
+// Signin-popup, Login-popup 페이지 보이기, 숨기기 시작
+function LoginPopupOpen() {
+  var LoginPopup = document.querySelector('.login-popup-background');
+  LoginPopup.style.visibility = "visible";
+  LoginPopup.style.opacity = '1';
+}
+
+function LoginPopupClose() {
+  var LoginPopup = document.querySelector('.login-popup-background');
+  LoginPopup.style.visibility = "hidden";
+  LoginPopup.style.opacity = '0';
+}
+
+function SigninPopupOpen() {
+  var SigninPopup = document.querySelector('.signin-popup-background');
+  SigninPopup.style.visibility = "visible";
+  SigninPopup.style.opacity = '1';
+}
+
+function SigninPopupClose() {
+  var SigninPopup = document.querySelector('.signin-popup-background');
+  SigninPopup.style.visibility = "hidden";
+  SigninPopup.style.opacity = '0';
+}
+
+function SearchPopupOpen() {
+  var SigninPopup = document.querySelector('.search-popup-background');
+  SigninPopup.style.visibility = "visible";
+  SigninPopup.style.opacity = '1';
+}
+
+function SearchPopupClose() {
+  var LoginPopup = document.querySelector('.search-popup-background');
+  LoginPopup.style.visibility = "hidden";
+  LoginPopup.style.opacity = '0';
+}
+// Signin-popup, Login-popup 페이지 보이기, 숨기기 끝
+
+// left-coment 박스 호출
+function showCategory(categoryId) {
+  var categoryContents = document.querySelectorAll('.category-content');
+  for (var i = 0; i < categoryContents.length; i++) {
+      categoryContents[i].style.display = 'none';
+  }
+
+  var selectedCategory = document.getElementById(categoryId);
+  if (selectedCategory) {
+      selectedCategory.style.display = 'block';
+  }
+}
+
+// 찜목록 JS
+function addToCart() {
+  const cartSection = document.getElementById('장바구니');
+  cartSection.style.display = 'block'; 
+  cartSection.scrollIntoView({ behavior: 'smooth' });
+  if (!document.querySelector('.divider')) {
+    createDivider();
+  }
+}
+function createDivider() {
+  const divider = document.createElement('div');
+  divider.classList.add('divider'); 
+  const favoriteList = document.getElementById('찜한 목록');
+  favoriteList.parentNode.insertBefore(divider, favoriteList.nextSibling);
+}
+function checkout() {
+  window.location.href = 'pay.html';
+}
+// 찜목록 JS 종료
+// 주문 내역 JS 없음 X
+
+// 회원정보 
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  
+  var user1 = {
+      name: "kim",
+      password: "jun"
+    };
+    var user2 = {
+      name: "ningdol",
+      password: "qwer2024!"
+    };
+
+  if ((username === "bae" && password === "dong") || 
+      (username === user1.name && password === user1.password) ||
+      (username === user2.name && password === user2.password)) {
+      // 로그인 성공
+      document.getElementById("change-info").style.display = "block"; 
+      document.getElementById("change-password").style.display = "block"; 
+      alert("로그인 되었습니다.");
+  } else {
+      // 로그인 실패
+      alert("아이디 또는 비밀번호가 올바르지 않습니다.");
+  }
+}
+
+function changeShippingAddress() {
+  var newShippingAddress = document.getElementById("new-shipping-address").value;
+  alert("배송지 주소가 변경되었습니다.");
+}
+
+function changePassword() {
+  var newPassword = document.getElementById("new-password").value;
+  var confirmNewPassword = document.getElementById("confirm-new-password").value;
+  if (newPassword === confirmNewPassword) {
+      alert("비밀번호가 변경되었습니다.");
+  } else {
+      alert("새로운 비밀번호와 확인 비밀번호가 일치하지 않습니다.");
+  }
+}
+//회원 정보 종료
+
+
+// 계정 탈퇴
+function showConfirmation() {
+  if (confirm("계정을 탈퇴하시겠습니까?")) {
+      window.location.href = "main-page-logout.html";
+  }
+}
+// 계정탈퇴종료
